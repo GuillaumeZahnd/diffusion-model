f0acf1dc0e243806ce1520e68355731c09a99f36
guillaume <guillaume@canaveral>
2022-11-02 08:11:46 +0100

    Run one

--- package_parameters/get_parameters.py
+++ package_parameters/get_parameters.py
@@ -6,7 +6,7 @@ from package_parameters.parameters import Parameters
 def get_parameters():
 
   return Parameters(
-    experiment_id       = 'run_zero',
+    experiment_id       = 'run_one',
     dataset_trn_path    = '/home/guillaume/DATASETS/afhq/train/',
     dataset_val_path    = '/home/guillaume/DATASETS/afhq/val/',
     results_path        = '/home/guillaume/RESULTS/diffusion-model/',
@@ -16,11 +16,11 @@ def get_parameters():
     ima_size            = 128,
     batch_size          = 4,
     nb_epochs           = 100,
-    optimizer_nickname  = 'ADAM',
+    optimizer_nickname  = 'SGD',
     learning_rate       = 1e-3,
     learning_momentum   = 0.99,
     scheduler_step_size = 1,
     scheduler_gamma     = 0.99,
-    loss_nickname       = 'MSE',
+    loss_nickname       = 'MAE',
     variance_schedule   = 'LINEAR',
     nb_timesteps        = 200)
