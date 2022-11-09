c09d686d4086c4791a4d7eb71aebeea8becdf416
guillaume <guillaume@canaveral>
2022-11-09 18:31:53 +0100

    Run one (resumed). Update the reverse generation routine.

--- package_routines/routine_reverse_generation.py
+++ package_routines/routine_reverse_generation.py
@@ -7,30 +7,27 @@ from package_showcase.showcase_reverse_generation import showcase_reverse_genera
 
 def routine_reverse_generation(p, tdp, model, id_epoch = None):
 
-  nb_channels       = 1 if p.RGB_OR_GRAYSCALE == 'grayscale' else 3
-  batch_size        = 1
-  shape             = (batch_size, nb_channels, p.IMA_SIZE, p.IMA_SIZE)
-  id_image_in_batch = 0
-  nb_images         = 9
-  generated_images  = []
-
-  # TODO: "p.BATCH_SIZE" is irrelevant here, and slows down the process, should be 1
+  batch_size       = 9 # Re-definition of the batch size (TODO: Use eiher this solution, or "batch_size=1" with a For loop over "nb_images")
+  nb_channels      = 1 if p.RGB_OR_GRAYSCALE == 'grayscale' else 3
+  shape            = (batch_size, nb_channels, p.IMA_SIZE, p.IMA_SIZE)
+  generated_images = []
 
   with torch.no_grad():
 
-    for id_image in tqdm(range(nb_images), desc = 'Reverse generation', total = nb_images):
-      img = torch.randn(shape, device = p.DEVICE)
+    img = torch.randn(shape, device = p.DEVICE)
 
-      for id_timestep in reversed(range(0, p.NB_TIMESTEPS)):
-        img = tdp.p_sample(
-          model   = model,
-          x       = img,
-          t       = torch.full((p.BATCH_SIZE, ), id_timestep, device = p.DEVICE, dtype = torch.long),
-          t_index = id_timestep)
-      generated_images.append(trafo_tensor_to_pil(img, id_image_in_batch))
+    for id_timestep in tqdm(reversed(range(0, p.NB_TIMESTEPS)), desc = 'Reverse generation', total = p.NB_TIMESTEPS):
+      img = tdp.p_sample(
+        model   = model,
+        x       = img,
+        t       = torch.full((batch_size, ), id_timestep, device = p.DEVICE, dtype = torch.long),
+        t_index = id_timestep)
+
+  for id_image_in_batch in range(batch_size):
+    generated_images.append(trafo_tensor_to_pil(img, id_image_in_batch))
 
   showcase_reverse_generation(
     p                = p,
     generated_images = generated_images,
-    nb_images        = nb_images,
+    nb_images        = batch_size,
     id_epoch         = id_epoch)
