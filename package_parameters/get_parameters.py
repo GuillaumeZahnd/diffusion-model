e33bd4581d8d1a397d510ee091cf149f443033d2
guillaume <guillaume@canaveral>
2023-01-18 22:13:23 +0100

    renaissance_02

--- package_parameters/get_parameters.py
+++ package_parameters/get_parameters.py
@@ -7,21 +7,21 @@ def get_parameters():
 
   return Parameters(
     # Experiment
-    experiment_id          = 'afhq_08', # Nickname to uniquely identify the run
-    training_bootstrap     = 'USE_PRETRAINING', # ['NEW_TRAINING', 'RESUME_TRAINING', 'USE_PRETRAINING']
+    experiment_id          = 'renaissance_02', # Nickname to uniquely identify the run
+    training_bootstrap     = 'NEW_TRAINING', # ['NEW_TRAINING', 'RESUME_TRAINING', 'USE_PRETRAINING']
     results_path           = '/home/guillaume/RESULTS/diffusion-model/', # Path to store the results
-    pretraining_path       = '/home/guillaume/RESULTS/diffusion-model/afhq_07/trained_model/', # Path to the checkpoint of a pre-trained model
-    pretraining_name       = 'model_min_val_loss_afhq_07.pt', # Name (including ".pt" extension) of the checkpoint of a pre-trained model
+    pretraining_path       = '', # Path to the checkpoint of a pre-trained model
+    pretraining_name       = '', # Name (including ".pt" extension) of the checkpoint of a pre-trained model
     # Dataset
-    dataset_trn_path       = '/home/guillaume/DATASETS/stargan-v2/data/afhq_v2/train/', # Path to the training set folder
-    dataset_val_path       = '/home/guillaume/DATASETS/stargan-v2/data/afhq_v2/test/', # Path to the validation set folder
-    ima_extension          = '.png', # ['.jpg', '.png', ...] File format
+    dataset_trn_path       = '/home/guillaume/DATASETS/artbench/train/renaissance/', # Path to the training set folder
+    dataset_val_path       = '/home/guillaume/DATASETS/artbench/test/renaissance/', # Path to the validation set folder
+    ima_extension          = '.jpg', # ['.jpg', '.png', ...] File format
     nb_samples_limit       = math.inf, # ['math.inf', 2000, ...] Maximal number of samples per epoch
     rgb_or_grayscale       = 'rgb', # ['RGB', 'GRAYSCALE'] Indicate whether images are to be treated as RGB or grayscale
     ima_size               = 128, # Image size after resizing
     cropping_method        = 'CENTER_CROP_THEN_RESIZE', # ['CENTER_CROP_THEN_RESIZE', 'RANDOM_CROP'] Indicate how images shall be cropped
     # Training
-    backbone               = 'RESNET', # ['RESNET', 'CONVNEXT']
+    backbone               = 'CONVNEXT', # ['RESNET', 'CONVNEXT']
     nb_epochs              = 500, # Number of training epochs
     batch_size             = 16, # Batch size
     optimizer_nickname     = 'SGD', # ['ADAM', 'SGD']
